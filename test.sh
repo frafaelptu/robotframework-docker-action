4bed72340cd83c557c22fd221f97ee7db574a8b4
Francisco Rafael Gomes Silva <frafaelptu@yahoo.com.br>
2023-10-20 09:35:16 -0300

    fix

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 # Create reports folder
 REPORTS_DIR=$(pwd)/$ROBOT_REPORTS_DIR
 TESTS_DIR=$(pwd)/$ROBOT_TESTS_DIR
-#RESOURCES_DIR=$(pwd)/$ROBOT_RESOURCES_DIR
+RESOURCES_DIR=$(pwd)/$ROBOT_RESOURCES_DIR
 sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
 #Variables
@@ -10,10 +10,10 @@ docker run --shm-size=$ALLOWED_SHARED_MEMORY \
   -e ROBOT_THREADS=$ROBOT_THREADS \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
   -e ROBOT_OPTIONS="$ROBOT_OPTIONS" \
-  #-e ROBOT_USERNAME_LOGIN="$ROBOT_USERNAME_LOGIN" \
- # -e ROBOT_PASSWORD_USER="$ROBOT_PASSWORD_USER" \
+  -e ROBOT_USERNAME_LOGIN="$ROBOT_USERNAME_LOGIN" \
+  -e ROBOT_PASSWORD_USER="$ROBOT_PASSWORD_USER" \
   -v $REPORTS_DIR:/opt/robotframework/reports:Z \
   -v $TESTS_DIR:/opt/robotframework/tests:Z \
- # -v $RESOURCES_DIR:/opt/robotframework/resources:Z \  
+  -v $RESOURCES_DIR:/opt/robotframework/resources:Z \  
   --user $(id -u):$(id -g) \
   $ROBOT_RUNNER_IMAGE
