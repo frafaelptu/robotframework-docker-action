7f63465801a88e0fb32f5bad4bc9e2b29109dc9a
Francisco Rafael Gomes Silva <frafaelptu@yahoo.com.br>
2023-10-22 07:51:56 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ sudo mkdir $REPORTS_DIR && sudo chmod 777 $REPORTS_DIR
 
 #Variables
 #--user $(id -u):$(id -g) \
-docker run --shm-size=$ALLOWED_SHARED_MEMORY \
+docker run --user $(id -u):$(id -g) --shm-size=$ALLOWED_SHARED_MEMORY \
   -e BROWSER=$BROWSER \
   -e ROBOT_THREADS=$ROBOT_THREADS \
   -e PABOT_OPTIONS="$PABOT_OPTIONS" \
